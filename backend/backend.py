32167ddec14aa20dba9705fa9b9bcd997c814748
Michael Arthur <87254861+MichaelArthur224@users.noreply.github.com>
2025-03-08 14:57:02 -0800

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -60,5 +60,6 @@ def search_user():
     return jsonify(filtered_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
 #python backend.py
